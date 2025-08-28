81bb5679b30fa39f06144ff98d577dd694d46713
Gregory Klaus <gregory.klaus2015@gmail.com>
2025-08-27 21:29:24 -0300

    Add files via upload
    Primeiro dia!

---
+++ Eduarda(Proa)/Aula01_Mentoria.py
@@ -0,0 +1,43 @@
+# <-- comentario em python
+
+#EPS
+# E -- (Entrada- onde o usuario vai digitar algo)
+# P -- (Processamento - partde da logica/caluculo)
+# S--  (Saida a mensagem que mandamos para o usuario)
+#print("Primeira saida em python") # Entre "" nossa mensagem (Saida)
+#input() # entrada 
+# int - tipo de dado inteiro numeros que tiverem . não entra
+# float - numeros com casas decimais
+#Exercicio
+#saida
+print("Bem vindo, calculadora de soma!")
+#entrada
+numero1 = int(input("Digite um numero para a soma:"))
+numero2 = float(input("Digite o segundo numero para a soma:"))
+#processamento
+resultado = numero1 + numero2
+#saida
+print ("O resultado da soma do numero:",numero1,"+",numero2,"e:",resultado)
+print (f"O resultado da soma do numero: {numero1} + {numero2} e: {resultado}")
+
+
+
+# 1 - programa que realiza a soma de dois valores  (+)
+# 2 - programa que realiza a subtração de dois valores (-)
+# 3 - programa que realiza a multiplicação de dois valores (*)
+# 4 - programa que realiza a divisão (/)
+# 5 - programa que exibe uma saudação
+# 6 - programa que recebe o nome do usuario e mostra um "Ola seja be vindo e o nome do usario"
+# 7 - programa que calcula area de um quadrado
+# 7 - programa que calcula area de um retangulo
+# 8 - programa que calcula area de triangulo
+# 9 - programa que calcula perimetro de um circulo
+# 10 - programa que calcula area de um losangolo 
+# 11 - programa que calcula area de um hexagono
+# 12 - prograa que calcula area de um pentagono
+
+
+
+
+
+
